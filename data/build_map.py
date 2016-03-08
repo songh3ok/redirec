f5fb56110673948bbad51f31901999fd3250d30f
lexifdev <sl@lxf.kr>
2016-03-08 22:58:58 +0900

    아무리 1회용이라도 그렇지

--- data/build_map.py
+++ data/build_map.py
@@ -13,14 +13,14 @@ for line in csv.reader(response.raw, delimiter='\t'):
         continue
 
     cp949_str, utf8_str, comment = line
+
     if cp949_str.strip() and utf8_str.strip():
         utf8 = unichr(int(utf8_str, 0)).encode('utf8')
         cp949 = urllib.quote(struct.pack(">H", int(cp949_str, 0)))
         map[utf8] = cp949
 
 
-var = 'var charTable = %s;' % json.dumps(map)
+charTable = 'var charTable = %s;' % json.dumps(map)
 commonScript = open('src.js').read()
 
-js_file = open('build.js', 'w+')
-js_file.write(var + commonScript)
+open('build.js', 'w+').write(charTable + commonScript)
